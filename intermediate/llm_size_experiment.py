020015ce4a1edf44e64f6df004aef4e3c4c4de17
Sebislaw <sebapx2003@gmail.com>
2025-05-30 12:12:04 +0200

    intermediate model integration

--- intermediate/llm_size_experiment.py
+++ intermediate/llm_size_experiment.py
@@ -13,12 +13,14 @@ class LLMSizeExperiment:
         # Classifier outputs
         self.classifier_predicted_labels: Dict[str, List[str]] = {}
         self.classifier_accuracy: Dict[str, float] = {}
+        self.classifier_confidence: Dict[str, List[float]] = {}
         # LLM concept guessing outputs and metrics
         self.llm_predicted_concepts: Dict[str, str] = {}
         self.llm_concept_accuracy: Dict[str, float] = {}
         # LLM simulation outputs and metrics
         self.llm_simulation_predicted_labels: Dict[str, List[str]] = {}
         self.llm_simulation_accuracy: Dict[str, float] = {}
+        self.llm_direct_prediction_accuracy: Dict[str, float] = {}
 
     def run(
             self,
@@ -27,21 +29,24 @@ class LLMSizeExperiment:
             y_train: List[str],
             x_test: List[str],
             y_test: List[str],
+            x_val: List[str],
+            y_val: List[str],
             dataset_name: str,
             concept: str,
             concept_keywords: List[str],
+
             classifier_name: str,
             classifier: BaseModel,
-            llm_models: Dict[str, "LLM"],
+            train_classifier: bool,
+            classifier_train_arguments: Dict[str, int],
 
+            llm_models: Dict[str, BaseModel],
             prompt_header_llm_concept: str,
             prompt_content_llm_concept: str,
             prompt_tail_llm_concept: str,
-
             prompt_header_llm_train: str,
             prompt_content_llm_train: str,
             prompt_tail_llm_train: str,
-
             prompt_llm_simulation: str,
     ) -> tuple[DataFrame, DataFrame, DataFrame]:
 
@@ -51,16 +56,23 @@ class LLMSizeExperiment:
         # Classifier outputs
         self.classifier_predicted_labels: Dict[str, List[str]] = {}
         self.classifier_accuracy: Dict[str, float] = {}
+        self.classifier_confidence: Dict[str, List[float]] = {}
         # LLM concept guessing outputs and metrics
         self.llm_predicted_concepts: Dict[str, str] = {}
         self.llm_concept_accuracy: Dict[str, float] = {}
         # LLM simulation outputs and metrics
         self.llm_simulation_predicted_labels: Dict[str, List[str]] = {}
         self.llm_simulation_accuracy: Dict[str, float] = {}
+        self.llm_direct_prediction_accuracy: Dict[str, float] = {}
 
         # 1) classifier prediction
-        classifier_predicted_labels = [classifier.predict(text) for text in x_test]
+        if train_classifier:
+            classifier.train(x_train, y_train, x_val, y_val, **classifier_train_arguments)
+        raw_predictions = [classifier.predict(text) for text in x_test]
+        classifier_predicted_labels = [list(d.keys())[0] for d in raw_predictions]
+        classifier_predicted_confidences = [list(d.values())[0] for d in raw_predictions]
         self.classifier_predicted_labels[classifier_name] = classifier_predicted_labels
+        self.classifier_confidence[classifier_name] = classifier_predicted_confidences
         self.classifier_accuracy[classifier_name] = (
             sum(pred == y for pred, y in zip(classifier_predicted_labels, y_test))/ len(y_test)
         )
@@ -74,7 +86,7 @@ class LLMSizeExperiment:
             for x, y in zip(x_test, y_test):
                 prompt_content += prompt_content_llm_concept.format(x_test=x, y_test=y)
             prompt = prompt_header_llm_concept + prompt_content + prompt_tail_llm_concept
-            llm_concept_prediction = llm_model.predict_concept(input_text=prompt)
+            llm_concept_prediction = "Predicted concept" #llm_model.predict_concept(input_text=prompt) TODO
             self.llm_predicted_concepts[name] = llm_concept_prediction
             self.llm_concept_accuracy[name] = float(concept.lower() in llm_concept_prediction.lower()
                                                     or any(kw.lower() in llm_concept_prediction.lower()
@@ -97,6 +109,9 @@ class LLMSizeExperiment:
             self.llm_simulation_accuracy[name] = sum(simulation_label == classifier_label
                                                      for simulation_label, classifier_label in zip(llm_simulation_predicted_labels,
                                                      classifier_predicted_labels)) / len(llm_simulation_predicted_labels)
+            self.llm_direct_prediction_accuracy[name] = sum(simulation_label == true_label
+                                                     for simulation_label, true_label in zip(llm_simulation_predicted_labels,
+                                                    y_test)) / len(llm_simulation_predicted_labels)
 
         # 4) build model DataFrame
         rows = []
@@ -109,6 +124,7 @@ class LLMSizeExperiment:
                 'classifier_accuracy': self.classifier_accuracy[classifier_name],
                 'llm_concept_accuracy': self.llm_concept_accuracy.get(llm_name, 0.0),
                 'llm_simulation_accuracy': self.llm_simulation_accuracy.get(llm_name, 0.0),
+                'llm_direct_prediction_accuracy': self.llm_direct_prediction_accuracy.get(llm_name, 0.0),
                 'prompt_header_llm_concept': prompt_header_llm_concept,
                 'prompt_content_llm_concept': prompt_content_llm_concept,
                 'prompt_tail_llm_concept': prompt_tail_llm_concept,
@@ -125,9 +141,11 @@ class LLMSizeExperiment:
         for llm_name, _ in llm_models.items():
             for (x, y,
                  classifier_predicted_label,
-                 llm_simulation_predicted_label) in zip(x_test, y_test,
+                 llm_simulation_predicted_label,
+                 classifier_predicted_label_confidence) in zip(x_test, y_test,
                                                         self.classifier_predicted_labels[classifier_name],
-                                                        self.llm_simulation_predicted_labels[llm_name]):
+                                                        self.llm_simulation_predicted_labels[llm_name],
+                                                        self.classifier_confidence[classifier_name]):
                 rows.append({
                     'run_id': self.run_number,
                     'dataset_name': dataset_name,
@@ -136,30 +154,30 @@ class LLMSizeExperiment:
                     'x_test': x,
                     'y_test': y,
                     'classifier_predicted_label': classifier_predicted_label,
+                    'classifier_predicted_label_confidence': classifier_predicted_label_confidence,
                     'llm_simulation_predicted_label': llm_simulation_predicted_label
                 })
         prediction_statistics = pd.DataFrame(rows)
 
         # 6) build data DataFrame
         rows = []
-        for llm_name, _ in llm_models.items():
-            for counts in [Counter(y_train), Counter(y_test)]:
-                train_props = {label: count / len(y_train)
-                               for label, count in counts.items()}
-                train_avg_text_length = sum(len(txt) for txt in x_train) / len(x_train)
-                train_avg_word_count = sum(len(txt.split()) for txt in x_train) / len(x_train)
-                rows.append({
-                    'run_id': self.run_number,
-                    'dataset_name': dataset_name,
-                    'classifier_name': classifier_name,
-                    'llm_name': llm_name,
-                    'partition': 'train',
-                    'num_samples': len(x_train),
-                    'label_counts': str(dict(counts)),
-                    'label_proportions': str({k: round(v, 2) for k, v in train_props.items()}),
-                    'avg_text_length': round(train_avg_text_length, 1),
-                    'avg_word_count': round(train_avg_word_count, 1)
-                })
+        for x_data, y_data, name_data in zip([x_train, x_val, x_test], [y_train, y_val, y_test], ['train', 'val', 'test']):
+            counts = Counter(y_data)
+            train_props = {label: count / len(y_data)
+                           for label, count in counts.items()}
+            avg_text_length = sum(len(txt) for txt in x_data) / len(x_data)
+            avg_word_count = sum(len(txt.split()) for txt in x_data) / len(x_data)
+            rows.append({
+                'run_id': self.run_number,
+                'dataset_name': dataset_name,
+                'classifier_name': classifier_name,
+                'partition': name_data,
+                'num_samples': len(x_data),
+                'label_counts': str(dict(counts)),
+                'label_proportions': str({k: round(v, 2) for k, v in train_props.items()}),
+                'avg_text_length': round(avg_text_length, 1),
+                'avg_word_count': round(avg_word_count, 1)
+            })
         data_statistics = pd.DataFrame(rows)
 
         return model_statistics, prediction_statistics, data_statistics
